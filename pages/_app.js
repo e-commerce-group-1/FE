bc4aae61b8620809a0ba671343557baf3bb52cf5
diydiyydiyyy <chalimatus.sadiyah.098@gmail.com>
2022-02-15 14:19:24 +0700

    install dependency

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+import 'semantic-ui-css/semantic.min.css'
+import Navbar from '../components/Navbar'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return 
+  <>
+  <Navbar />
+  <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
