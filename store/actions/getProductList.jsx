6f0149d320362b76c7ffca40f258c9769724b5d3
Rizki Awenk <adiwirizki207@gmail.com>
2022-02-21 05:11:24 +0700

    add cors headers

--- store/actions/getProductList.jsx
+++ store/actions/getProductList.jsx
@@ -3,7 +3,10 @@ import axios from "axios";
 export const getProducts = () =>{
     return (dispatch) => {
         axios
-        .get("http://3.1.211.120:8081/products")
+        .get("http://3.1.211.120:8081/products",{headers: { 
+            'Access-Control-Allow-Origin': '*', 
+            'Content-Type': 'application/json'
+          }})
         .then(({data}) => {
             dispatch(setProducts(data.data));
         })
